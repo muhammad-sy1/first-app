ce9091a481fbdb9bbd9f349f5ca180de1777b67e
Muhammad Alyousef <muhammadabdalrahman2003@gmail.com>
2025-08-31 17:36:43 +0300

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,27 @@ import Navbar from "./my-components/Navbar";
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="relative">
+        <Navbar />
+        <div className="h-[600px] w-full ">
+          <img
+            src="/banner.png"
+            alt="banner"
+            className="w-full h-full object-cover"
+          />
+          <div className="w-full h-full bg-white/60 absolute inset-0">
+            <div className="flex flex-col justify-center items-center gap-y-5 h-full">
+              <div className="text-my-green font-bold text-4xl">
+                Voyagez intelligemment et économisez davantage
+              </div>
+              <div className="to-my-black font-medium text-2xl">
+                Trouvez des chauffeurs de confiance et profitez de voyages
+                économiques.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/my-components/Navbar.jsx
+++ src/my-components/Navbar.jsx
@@ -1,11 +1,117 @@
-import React from 'react'
+import React from "react";
+import { SlPlus } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
 
 const Navbar = () => {
   return (
-    <nav className='w-full absolute bg-red-500 h-20'>
-      
+    <nav className="container absolute z-50 top-10 inset-x-0">
+      <div className="px-2 bg-gradient-to-r from-my-black/85 from-5% via-my-green/75 via-20% to-white/50 to-90% h-16 rounded-xl">
+        <div className="flex justify-between items-center h-full">
+          <div className="h-full flex justify-center items-center p-3">
+            <img src="/logo.png" alt="logo" className="h-full cursor-pointer" />
+          </div>
+          <div className="flex items-center gap-x-4 text-gray-400 ">
+            <Dialog>
+              <DialogTrigger>
+                <div className="flex items-center gap-x-2 transition-colors hover:text-my-black">
+                  <SlPlus className="text-2xl" />
+                  <span className="font-bold text-xl">Publish a ride</span>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <form>
+                <DialogTrigger asChild>
+                  <div
+                    variant="secondary"
+                    size="icon"
+                    className="cursor-pointer transition-colors hover:text-my-black"
+                  >
+                    <IoIosSearch className="text-3xl" />
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Search</DialogTitle>
+                    <DialogDescription>
+                      Click search when you're done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <Input
+                        id="name-1"
+                        name="name"
+                        placeholder="Type anything"
+                        // defaultValue="Type anything"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Search</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <span className="flex items-center gap-x-1 text-2xl transition-colors hover:text-my-black">
+                  <FaUserCircle />
+                  <MdKeyboardArrowDown />
+                </span>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
